bddb50f09f552448065d154d7b2609d7d6975626
Arya Banaeizadeh <arya11.b76@gmail.com>
2018-10-28 11:50:50 +0330

    + encryption

--- proxy.py
+++ proxy.py
@@ -1,36 +1,64 @@
 import socket
 import requests
-
 from _thread import *
+import base64
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 host = ''
 port = 5555
+key = '!!#pp73281'
 server_host = 'localhost'
 server_port = 8080
-def post_to_server(body):
-    # print('ni')
-    print(body)
-    requests.post('http://{0}:{1}'.format(server_host, server_port), body)
-def encrypt(text):
-    print('enc not implemented')
-    return text
-def client_thread(con):
-    con.send(str.encode('hello there!'))
+def recv_data(con):
+    reply = []
     while True:
-        data = con.recv(2048)
-        reply = data.decode('utf-8')
-        enc_data = encrypt(reply)
-        post_to_server(enc_data)
-        if not data: break
-        con.send(str.encode('works'))#str.encode(reply))
+        try:
+            con.settimeout(2.0)
+            data = con.recv(2048)
+            reply.append(data)
+        except socket.error as e:
+            break
+    reply = b''.join(reply)
+    print(reply)
+    return reply
+def post_to_server(body):
+    return requests.post('http://{0}:{1}'.format(server_host, server_port), body)
+def encrypt(text):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=b'1',
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(b'proxyRules!'))
+    f = Fernet(key)
+    text = f.encrypt(bytes(text, "utf-8")).decode("utf-8")
+    return text
+def client_thread(con,eh,ep):
+    data = con.recv(4096)
+    reply = data.decode('utf-8')
+    enc_data = encrypt(reply)
+    res = post_to_server(enc_data)
+    res_plain = 'HTTP/1.1 {status_code}\n\n{body}'.format(
+        status_code= res.status_code,
+        headers='\n'.join('{}: {}'.format(k, v) for k, v in res.headers.items()),
+        body=res.text,
+    )
+    print(res_plain)
+    con.sendall(res_plain.encode())
     con.close()
-
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 try:
     s.bind((host,port))
-    s.listen(5)
+    s.listen(10)
 except socket.error as e:
     print(str(e))
 while True:
     con, addr = s.accept()
-    print('connected to {0}:{1}'.format(addr[0],addr[1]))
-    start_new_thread(client_thread,(con,))
+    print('connected to {0}:{1}'.format(addr[0], addr[1]))
+    end_host = addr[0]
+    end_port = addr[1]
+    start_new_thread(client_thread, (con,end_host,end_port,))
--- server.py
+++ server.py
@@ -1,18 +1,80 @@
 import socket
 from _thread import *
-
+import sys
+import base64
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 def decrypt(data):
-    print('not implemented')
-    return data
-def server_thread(con):
-    con.send(str.encode('pajfe'))
+    print(data)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=b'1',
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(b'proxyRules!'))
+    f = Fernet(key)
+    token = f.decrypt(bytes(data, "utf-8")).decode("utf-8")
+    return token
+def validate_post(req_token):
+    # if req_token.find('POST') == -1: return False
+    return False
+def get_req_info(req):
+    req_token = req.split('\n')
+    body_enc = '\n'.join(req_token[req_token.index('\r') + 1:])
+    body_token = decrypt(body_enc).split('\n')
+    print(body_token)
+    for i in body_token:
+        if 'accept-encoding' in i:
+            body_token.remove(i)
+    url = body_token[0].split(' ')[1]
+    body = '\n'.join(body_token)
+    return url , body
+def send_to_url(url,body):
+    server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s_port = 80
+    if url[-1] == '/':
+        url = url[:-1]
+    if 'http://' in url:
+        url = url[7:]
+    elif 'https://' in url:
+        url = url[8:]
+    if ':' in url:
+        temp_url = url[:url.index(':')]
+        s_port = int(url[url.index(':')+1:])
+        url = temp_url
+    try:
+        server_socket.connect((url,s_port))
+        server_socket.send(str.encode(body))
+    except socket.error as e:
+        print(str(e))
+        sys.exit(1)
+    reply = []
     while True:
-        data = con.recv(2048)
-        reply = data.decode('utf-8')
-        print(reply)
-        if not data: break
-        con.send(str.encode('works'))#str.encode(reply))
+        try:
+            server_socket.settimeout(3.0)
+            data = server_socket.recv(2048)
+            try:
+                reply.append(data)
+            except UnicodeDecodeError as u:
+                pass
+        except socket.timeout as e:
+            break
+    return b''.join(reply)
+def server_thread(con):
+    data = con.recv(4096)
+    reply = data.decode('UTF-8')
+    # print(req)
+    url, body = get_req_info(reply)
+    res = send_to_url(url,body)
+    print('yay')
+    print(res)
+    con.send(res)
+    print('yay 2')
     con.close()
 host = ''
 port = 8080
